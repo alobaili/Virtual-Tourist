f744deef6f5d065108b3e41400138a3007825985
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-24 23:05:21 +0300

    Add new view

---
+++ Virtual Tourist/View Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Abdulaziz AlObaili on 24/02/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
