b26d9bf873b38c95c1bcbc28b809a39ec0ffdad5
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-25 00:03:36 +0300

    Add content to photo album view

--- Virtual Tourist/View Controllers/PhotoAlbumViewController.swift
+++ Virtual Tourist/View Controllers/PhotoAlbumViewController.swift
@@ -9,17 +9,30 @@
 import Foundation
 import UIKit
 import CoreData
+import MapKit
 
 class PhotoAlbumViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewCell: UICollectionViewCell!
+    
     
     var dataController: DataController!
     var pin: Pin!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+}
+
+extension PhotoAlbumViewController: UICollectionViewDelegate {
+    
 }
