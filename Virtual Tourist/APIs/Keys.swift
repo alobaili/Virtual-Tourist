c22333a15eb94e8fa81822467a99c06e760648f8
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-26 02:00:05 +0300

    Add Keys.swift

---
+++ Virtual Tourist/APIs/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  Virtual Tourist
+//
+//  Created by Abdulaziz AlObaili on 25/02/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    static let flickrAPIKey: String = "56d3b0ffbb62c2abda740147deca2b7f"
+    static let flickrAPISecret: String = "978eb6b4931016d4"
+}
